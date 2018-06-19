ba98f222e1fc5f9e102e08e4c5b51f0349986715
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-06-19 04:12:35 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use \App\Item;
 
   class ItemsController extends Controller
@@ -33,5 +35,16 @@ use \App\Item;
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
   }
