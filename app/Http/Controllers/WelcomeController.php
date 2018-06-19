54ab604cd371cc3b78206959fedf58939053b96c
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-06-19 01:17:03 +0000

    rakuten api

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -15,7 +15,7 @@ class WelcomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index()    
     {
         return view('welcome');
     }
