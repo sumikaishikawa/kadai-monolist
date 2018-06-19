a22eea2d226a46e16f933f0b193d13506288da87
sumika.ishikawa <sumika.ishikawa@rakuten.com>
2018-06-19 05:40:08 +0000

    have

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -13,18 +13,15 @@
                             @else
                                 <p class="item-title">{{ $item->name }}</p>
                             @endif
-                            <div class="btn-group-inline"  role="group" aria-label="...">
-                                @if (Auth::check())
-                                    @include('items.want_button', ['item' => $item])
-                                @endif
-                                @if (Auth::check())
-                                    @include('items.want_button', ['item' => $item])
-                                @endif
+                            <div class="buttons text-center">
+                                    @if (Auth::check())
+                                       @include('items.want_button', ['item' => $item])
+                                       @include('items.have_button', ['item' => $item])
+                                    @endif
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+               </div>
         @endforeach
     </div>
 @endif
